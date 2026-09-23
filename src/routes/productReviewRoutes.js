24245f7a873dae4583cd0fbd8e2fe1ce48320bfd
bramhakotigoud <bramhakoti3@gmail.com>
2026-09-23 19:07:59 +0530

    resolved product review system in orders

--- src/routes/productReviewRoutes.js
+++ src/routes/productReviewRoutes.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const router = express.Router();
 
 const {
   createReview,
+  createOrderReview,
   getProductReviews,
 } = require("../controllers/productReviewController");
 
+const router = express.Router();
+
+/* --------------------------------
+   PRODUCT REVIEW
+-------------------------------- */
 router.post("/:productId/reviews", createReview);
 
+/* --------------------------------
+   ORDER REVIEW
+   One review -> all products
+   in that order
+-------------------------------- */
+router.post("/orders/:orderId/review", createOrderReview);
+
+/* --------------------------------
+   GET PRODUCT REVIEWS
+-------------------------------- */
 router.get("/:productId/reviews", getProductReviews);
 
 module.exports = router;
